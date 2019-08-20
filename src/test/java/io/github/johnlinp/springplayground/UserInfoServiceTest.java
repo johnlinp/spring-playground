9651ad7a7d51bb5946a518d9ed1c78334127e705
John Lin <johnlinp@gmail.com>
2019-08-20 11:57:03 +0800

    Use ContextConfiguration to replace ContextHierarchy

--- src/test/java/io/github/johnlinp/springplayground/UserInfoServiceTest.java
+++ src/test/java/io/github/johnlinp/springplayground/UserInfoServiceTest.java
@@ -6,29 +6,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextHierarchy({
-        @ContextConfiguration(classes = TestSecurityContext.class),
-        @ContextConfiguration(classes = UserInfoServiceTest.Config.class)
-})
+@ContextConfiguration(classes = { TestSecurityContext.class, UserInfoServiceTest.Config.class })
 public class UserInfoServiceTest
 {
     @Configuration
+    @Import(UserInfoServiceImpl.class)
     public static class Config
     {
-        @Bean
-        public UserInfoService userInfoService ()
-        {
-            return new UserInfoServiceImpl();
-        }
     }
 
     @Autowired
